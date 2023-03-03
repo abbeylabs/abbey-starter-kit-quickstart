62e7a78586f0418f0d30fb36c5003dc097c4aa7a
Jeff Chao <jeffchao@me.com>
2023-03-03 12:35:16 -0800

    Add `output.location` stub.

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "abbey_grant_kit" "null_grant" {
   output = {
     # Replace with your own path pointing to where you want your access changes to manifest.
     # Path is an RFC 3986 URI, such as `github://{organization}/{repo}/path/to/file.tf`.
-    location = "github://abbeylabs/abbey-starter-kits/local/workflow-single-step/access.tf"
+    location = "github://organization/repo/access.tf"
     append = <<-EOT
       resource "null_resource" "null_grant_{{ $.data.system.abbey.primary_identity.username }}" {
       }
